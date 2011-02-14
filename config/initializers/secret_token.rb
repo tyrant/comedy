dca5642570986849a0dfc98ca543e623c3be0249
Mikey <pineutrino@gmail.com>
2011-02-14 18:58:28 +1300

    Added Devise stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comedy::Application.config.secret_token = 'f166f7c3b023fd018891dceac92ce004068c38687d52a3291e94a56aa643f78c4838632740c1cae04faa87914cb2efcc84febaff39f8e061290742b49fc968d4'
